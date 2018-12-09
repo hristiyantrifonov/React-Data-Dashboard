7e665c794e3797ba45d634afbedce8fb98455998
Hristiqn <hristiyantrifonov@gmail.com>
2018-12-09 22:06:38 +0000

    Finish styling the grid

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -2,17 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 
 import {AppContext} from "../App/AppProvider";
+import {SelectableTile} from "../Shared/Tile";
 
 export const CoinGridStyled = styled.div`
     display: grid;
     grid-template-columns: repeat(5, 1fr);
+    grid-gap: 15px;
 `
 
 const grid = () => {
     return <AppContext.Consumer>
         {({coinList}) =>
             <CoinGridStyled>
-                {Object.keys(coinList).map(coinKey => <div>{coinKey}</div>)}
+                {Object.keys(coinList).map(coinKey => <SelectableTile>{coinKey}</SelectableTile>)}
             </CoinGridStyled>}
     </AppContext.Consumer>
 }
